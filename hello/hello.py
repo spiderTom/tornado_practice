3f407a8ffee94118cf5909b8ee3e64743dde91b9
tomtubo <tomtubo@163.com>
2017-03-16 16:32:17 +0800

    third

---
+++ hello/hello.py
@@ -0,0 +1,30 @@
+#import sys
+#import os
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+
+
+define("port", default=8000, help="run on the given port", type=int)
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        greeting = self.get_argument('greeting', 'Hello')
+        self.write(greeting + ', friendly user!')
+
+if __name__ == "__main__":
+    #print sys.argv
+    #print len(sys.argv)
+    #print sys.argv[0]
+    #print sys.argv[1]
+
+    tornado.options.parse_command_line()
+    #print options.port
+    print "============="
+    app = tornado.web.Application(handlers=[(r"/", IndexHandler)])
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
